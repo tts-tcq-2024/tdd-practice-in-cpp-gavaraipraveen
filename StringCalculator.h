f642f9ee37d33b0038d9cd803b980df324aa2097
gavaraipraveen <113239024+gavaraipraveen@users.noreply.github.com>
2024-07-26 17:34:58 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -5,6 +5,7 @@ class StringCalculator {
 public:
     int add(const std::string& numbers);
 private:
+    int summation(const std::string& numbers);
     std::vector<int> convertToNumber(const std::string& numbers);
     std::vector<int> avoidNegativeNumbers(const std::vector<int>& numbers);
     void throwNegativeNumbersException(const std::vector<int>& negatives);

bb2357dde7d49d3a9ab544e8bf4fbbbe2d4f1a95
gavaraipraveen <113239024+gavaraipraveen@users.noreply.github.com>
2024-07-26 19:59:49 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -7,6 +7,8 @@ public:
 private:
     int summation(const std::string& numbers);
     std::vector<int> convertToNumber(const std::string& numbers);
+    std::string extractDelimiter(const std::string& numbers, std::string& sanitizedNumbers);
+    std::vector<int> splitNumbers(const std::string& sanitizedNumbers, const std::string& delimiter);
     std::vector<int> avoidNegativeNumbers(const std::vector<int>& numbers);
     void throwNegativeNumbersException(const std::vector<int>& negatives);
     int filterNumber(const std::string& numStr);

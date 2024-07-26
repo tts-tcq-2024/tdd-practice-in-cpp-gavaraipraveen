9ac7d0e48167748a2ffbed347470ef1ff1db3fc7
gavaraipraveen <113239024+gavaraipraveen@users.noreply.github.com>
2024-07-26 18:44:19 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,4 +9,5 @@ private:
     std::vector<int> convertToNumber(const std::string& numbers);
     std::vector<int> avoidNegativeNumbers(const std::vector<int>& numbers);
     void throwNegativeNumbersException(const std::vector<int>& negatives);
+    int filterNumber(const std::string& numStr);
 };

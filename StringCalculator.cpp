b49cf8a1ee14e4b89f61cfec303c5a4b7764bbd7
gavaraipraveen <113239024+gavaraipraveen@users.noreply.github.com>
2024-07-26 17:26:36 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -3,12 +3,18 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <stdexcept>
 
 int StringCalculator::add(const std::string& numbers) {
     if (numbers.empty()) {
         return 0;
     }
     std::vector<int> numberList = convertToNumber(numbers);
+    std::vector<int> negatives = filterNegativeNumbers(numberList);
+
+    if (!negatives.empty()) {
+        throwNegativeNumbersException(negatives);
+    }
     int add = 0;
     for (int num : numberList) {
         add += num;
@@ -27,4 +33,14 @@ std::vector<int> StringCalculator::convertToNumber(const std::string& numbers) {
     }
 
     return numList;
+}
+
+std::vector<int> StringCalculator::avoidNegativeNumbers(const std::vector<int>& numbers) {
+    std::vector<int> negatives;
+    for (int num : numbers) {
+        if (num < 0) {
+            negatives.push_back(num);
+        }
+    }
+    return negatives;
 }
